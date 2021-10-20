7d22d5c782c79d1a79ff926d01de435819d88802
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-20 15:53:50 +0900

    Feat:add 갤러리에서 사진선택 하면 Collection View 에서 보여지는 기능
    2021-10-20
    photosUI를 이용해서 갤러리에서 사진 선택 하면 collection view에서 선택된 사진들이 보여지는 기능 구현

--- PhotoGalleryApp/PhotoGalleryApp/PhotoCell.swift
+++ PhotoGalleryApp/PhotoGalleryApp/PhotoCell.swift
@@ -6,8 +6,32 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoCell : UICollectionViewCell{
     
-    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView!{
+        didSet{
+            photoImageView.contentMode = .scaleAspectFill
+        }
+    }
+    
+    func loadImage(asset:PHAsset){
+        
+        let imageManager = PHImageManager()
+        let scale = UIScreen.main.scale
+        let imageSize = CGSize(width: 150 * scale, height: 150 * scale)
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .fastFormat
+        imageManager.requestImage(for: asset, targetSize: imageSize, contentMode: .aspectFill, options: options) { image, info in
+            print(info)
+            print(info?[PHImageResultIsDegradedKey] as? Bool)
+            if (info?[PHImageResultIsDegradedKey] as? Bool) == true{
+                self.photoImageView.image = image //저화질
+            }else{
+                //고화질
+            } //option에서 deliveryMode 를 fastFormat으로 값을 설정하면 info dic에서 PHImageResultIsDegradedKey값이 1일줄 알았는데 0 이 나왔다.
+        }
+    }
 }
+
--- PhotoGalleryApp/PhotoGalleryApp/ViewController.swift
+++ PhotoGalleryApp/PhotoGalleryApp/ViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
     
+    var fetchResults: PHFetchResult<PHAsset>?
     @IBOutlet weak var photoCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +27,7 @@ class ViewController: UIViewController {
         photoCollectionView.collectionViewLayout = layout
     }
     func makeNavigationItem(){
-        let photoItem = UIBarButtonItem(image: UIImage(systemName: "photo.fill"), style: .done, target: self, action: #selector(showGallery))
+        let photoItem = UIBarButtonItem(image: UIImage(systemName: "photo.fill"), style: .done, target: self, action: #selector(checkPermission))
         photoItem.tintColor = .black.withAlphaComponent(0.7)
         
         self.navigationItem.rightBarButtonItem = photoItem
@@ -37,26 +39,71 @@ class ViewController: UIViewController {
     }
 }
 
-// MARK: bar button actions
+// MARK: - bar button actions
 extension ViewController{
-    @objc func showGallery(sender : UIBarButtonItem){
-        
+    @objc func checkPermission(){
+        if PHPhotoLibrary.authorizationStatus() == .authorized || PHPhotoLibrary.authorizationStatus() == .limited{ // authorized -> 사용자가 명시적으로 권한 부여 , limited -> 사용자가 이 앱에 제한된 권한을 승인 (선택한 몇개 만 사용 하겠다)
+            DispatchQueue.main.async {
+                self.showGallery()
+            }
+        }else if PHPhotoLibrary.authorizationStatus() == .denied{ //승인 거절 했을 경우
+            DispatchQueue.main.async {
+                self.showAuthorizationDeniedAlert()
+            }
+        }else if PHPhotoLibrary.authorizationStatus() == .notDetermined{ // 사용자가 앱의 인증상태를 설정하지 않은 경우 ex) 앱을 설치하고 처음 실행
+            PHPhotoLibrary.requestAuthorization { status in
+                self.checkPermission()
+            }
+        }
+    }
+    func showGallery(){
+        let library = PHPhotoLibrary.shared() //singleton pattern
+        var configuration = PHPickerConfiguration(photoLibrary: library)
+        configuration.selectionLimit = 10
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    func showAuthorizationDeniedAlert(){
+        let alert = UIAlertController(title: "포토라이브러리의 접근 권환을 활성화 해주세요.", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "닫기", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "설정으로 가기", style: .default, handler: { action in
+            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+            if UIApplication.shared.canOpenURL(url){
+                UIApplication.shared.open(url, options: [:],completionHandler: nil)
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     @objc func refresh(sender : UIBarButtonItem){
-        
+        self.photoCollectionView.reloadData()
     }
 }
 
-// MARK: UICollectionViewDataSource
+// MARK: - UICollectionView DataSource
 extension ViewController : UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return fetchResults?.count ?? 0
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        
+        if let asset = self.fetchResults?[indexPath.row]{
+            cell.loadImage(asset: asset)
+        }
         
         return cell
     }
-    
-    
 }
+
+// MARK: - PHPicker delegate
+extension ViewController : PHPickerViewControllerDelegate{
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        let identifiers = results.map{ $0.assetIdentifier ?? ""}
+        self.fetchResults = PHAsset.fetchAssets(withLocalIdentifiers: identifiers, options: nil)
+        self.photoCollectionView.reloadData()
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
