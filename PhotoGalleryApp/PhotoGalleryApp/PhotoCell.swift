cae63802d75ad0fee18fab374c815c7d8edb8ad6
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-20 16:01:57 +0900

    refactor:modify method requestImage
    2021 - 10 -20

--- PhotoGalleryApp/PhotoGalleryApp/PhotoCell.swift
+++ PhotoGalleryApp/PhotoGalleryApp/PhotoCell.swift
@@ -22,15 +22,17 @@ class PhotoCell : UICollectionViewCell{
         let scale = UIScreen.main.scale
         let imageSize = CGSize(width: 150 * scale, height: 150 * scale)
         let options = PHImageRequestOptions()
-        options.deliveryMode = .fastFormat
+        options.deliveryMode = .opportunistic
         imageManager.requestImage(for: asset, targetSize: imageSize, contentMode: .aspectFill, options: options) { image, info in
-            print(info)
-            print(info?[PHImageResultIsDegradedKey] as? Bool)
-            if (info?[PHImageResultIsDegradedKey] as? Bool) == true{
+            /*
+             if (info?[PHImageResultIsDegradedKey] as? Bool) == true{
                 self.photoImageView.image = image //저화질
             }else{
                 //고화질
             } //option에서 deliveryMode 를 fastFormat으로 값을 설정하면 info dic에서 PHImageResultIsDegradedKey값이 1일줄 알았는데 0 이 나왔다.
+             */
+            
+            self.photoImageView.image = image
         }
     }
 }
