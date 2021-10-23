c528b6c772ab1f3567f4894406557b3bae963742
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-23 16:35:51 +0900

    add custom buttom and button animation logic
    2021-10-23

--- Custom_Rotate_Button/Custom_Rotate_Button/RotateButton.swift
+++ Custom_Rotate_Button/Custom_Rotate_Button/RotateButton.swift
@@ -11,40 +11,45 @@ enum RotateType {
     case down
 }
 class RotateButton: UIButton {
-
+    
     var isUp = RotateType.down{
         didSet{
             changeDesign()
         }
     }
     
+    var selectedCallback : ((RotateType)->Void)?
     init() {
         super.init(frame: CGRect.zero)
         configure()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        configure()
     }
     private func configure(){
         self.addTarget(self, action: #selector(selectButton), for: .touchUpInside)
     }
-     
+    
     @objc private func selectButton(){
         if isUp == .up{
             isUp = .down
         }else{
             isUp = .up
         }
+        selectedCallback?(isUp)
     }
     
     private func changeDesign(){
-        if isUp == .down{
-            self.imageView?.transform = CGAffineTransform(rotationAngle: .pi)
-            self.backgroundColor = self.backgroundColor?.withAlphaComponent(0.3)
-        }else{
-            self.imageView?.transform = .identity
-            self.backgroundColor = self.backgroundColor?.withAlphaComponent(1)
+        UIView.animate(withDuration: 0.5) {
+            if self.isUp == .up{
+                self.imageView?.transform = CGAffineTransform(rotationAngle: .pi)
+                self.backgroundColor = self.backgroundColor?.withAlphaComponent(0.3)
+            }else{
+                self.imageView?.transform = .identity
+                self.backgroundColor = self.backgroundColor?.withAlphaComponent(1)
+            }
         }
     }
 }
--- Custom_Rotate_Button/Custom_Rotate_Button/ViewController.swift
+++ Custom_Rotate_Button/Custom_Rotate_Button/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var customButton: RotateButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        customButton.selectedCallback = {
+            upDownType in
+            print(upDownType)
+            if upDownType == .up{
+                print("여기서 해야할 일")
+            }
+        
         // Do any additional setup after loading the view.
+//
+//        let myButton = RotateButton()
+//        self.view.addSubview(myButton)
+//
+//
+//
+//        myButton.translatesAutoresizingMaskIntoConstraints = false
+//        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+//
+//        myButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//        myButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+//
+//        myButton.backgroundColor = .orange
+//        myButton.setTitle("my Custom button", for: .normal)
+//        myButton.setImage(UIImage(systemName: "arrowtriangle.down"), for: .normal)
+//
     }
-
-
 }
 
