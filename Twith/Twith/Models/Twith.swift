98ab52dd864d9b5b5ab190704bb56db5a157dbf4
zaezero97 <xornjs1423@gmail.com>
2022-01-24 16:10:25 +0900

    User,Twith 모델 생성

---
+++ Twith/Twith/Models/Twith.swift
@@ -0,0 +1,14 @@
+//
+//  Twith.swift
+//  Twith
+//
+//  Created by 재영신 on 2022/01/24.
+//
+
+import Foundation
+
+struct Twith: Codable {
+    let user: User
+    let content: String
+}
+
---
+++ Twith/Twith/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Twith
+//
+//  Created by 재영신 on 2022/01/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name: String
+    var account: String
+    
+    static var shared = User(name: "IOS개발자", account: "ios_developer")
+}
